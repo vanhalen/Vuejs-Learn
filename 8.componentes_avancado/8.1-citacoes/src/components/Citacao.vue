9df57c2f24b5f318e1d642aa03916f25759d294d
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2021-02-09 17:19:46 -0300

    chore: componentes avançados

---
+++ 8.componentes_avancado/8.1-citacoes/src/components/Citacao.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="citacao">
+    {{ texto }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fonte: String,
+    texto: String,
+    autor: String,
+  },
+};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
